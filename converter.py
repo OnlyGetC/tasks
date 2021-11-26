f9f9b82c3691347f1b3cdd2d8a0e6106c1e17085
Your Name <you@example.com>
2021-11-27 00:48:26 +0300

    Initial commit

---
+++ converter.py
@@ -0,0 +1,8 @@
+def converter(sec):
+    sec = sec % (24 * 3600)
+    hour = sec // 3600
+    sec %= 3600
+    min = sec // 60
+    sec %= 60
+    return "%02d:%02d:%02d" % (hour, min, sec)
+print(converter(69987654646))
